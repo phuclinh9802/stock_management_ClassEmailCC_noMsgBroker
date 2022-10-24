577ec55b6d8098ab6d37ef348a24769e112ec338
phucnguyen290198@gmail.com <phucnguyen290198@gmail.com>
2022-10-24 15:33:20 -0500

    change allowed hosts

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-azp7a1t580oey53%x*6%2uykd&t-d541bg+szl!d+_hse+suoq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
