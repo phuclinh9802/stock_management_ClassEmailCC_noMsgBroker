a18532aa10ba3d1660004deadcf02e0f609e1f46
phucnguyen290198@gmail.com <phucnguyen290198@gmail.com>
2022-10-24 16:17:30 -0500

    static root for production

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # if not DEBUG:
 #     STATIC_ROOT = ''
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static/')]
 
 
 # Default primary key field type
