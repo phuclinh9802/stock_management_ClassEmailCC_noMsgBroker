f68a8af4e46250be922648e13c574868d59adf32
phucnguyen290198@gmail.com <phucnguyen290198@gmail.com>
2022-10-24 15:29:47 -0500

    migrations + admin + models

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Role, Profile, Users, Watch_List, Stock, Preference, Stocks_list, New, Threshold
+
+admin.site.register(Role)
+admin.site.register(Profile)
+admin.site.register(Users)
+admin.site.register(Watch_List)
+admin.site.register(Stock)
+admin.site.register(Preference)
+admin.site.register(Stocks_list)
+admin.site.register(New)
+admin.site.register(Threshold)
