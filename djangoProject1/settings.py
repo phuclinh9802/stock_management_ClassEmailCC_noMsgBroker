b61191efbbe352d0eb0c960256e15ad23a8aedff
phucnguyen290198@gmail.com <phucnguyen290198@gmail.com>
2022-11-01 14:25:39 -0500

    settings.py logout-redirect-url

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages.apps.PagesConfig'
+    'pages.apps.PagesConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/dashboard'
+LOGOUT_REDIRECT_URL = '/accounts/login'
