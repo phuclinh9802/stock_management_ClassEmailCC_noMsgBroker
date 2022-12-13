79c2dd27a93d205ea99f58613cd1229874def3c0
Warlord09 <112018229+vidit009@users.noreply.github.com>
2022-12-13 11:40:25 -0600

    Threshold UI only

--- pages/forms.py
+++ pages/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import WatchList, StockNew
+from .models import WatchList, StockNew, Threshold
 
 
 # Create your forms here.
@@ -49,3 +49,13 @@ class NewsDeleteForm(forms.ModelForm):
        model = StockNew
        fields = ('stock_id', )
 
+class ThresholdForm(forms.ModelForm):
+
+   class Meta:
+       model = Threshold
+       fields = ('stock_id', 'threshold_percentage_change')
+
+class ThresholdDeleteForm(forms.ModelForm):
+   class Meta:
+       model = Threshold
+       fields = ('stock_id', )
